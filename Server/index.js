2b5b2eb3417080d49412df44f68f0b1c43615daa
Imran Ali <imranalishahani7@gmail.com>
2023-03-06 16:05:22 +0500

    some changing

--- Server/index.js
+++ Server/index.js
@@ -2,26 +2,35 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors : {
-        origin : "http://localhost:3500",
-        methods : ["get", "post"],
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on("connection",(socket)=>{
-    console.log(socket.id)
-})
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-io.on("disconnect",(socket)=>{
-    console.log("user disconnect", socket.id)
-})
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
-server.listen(3500,()=>{
-    console.log("welcome to start server");
-})
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server is Running start");
+});
